355f493da295719e997ae87855ed5066f0f591e7
Oleksandr Raspopov <oleksandr.raspopov@infopulse.com>
2022-04-03 21:30:58 +0300

    add(client): recoil: loader, user states

---
+++ client/src/recoil/loader/index.js
@@ -0,0 +1,6 @@
+import { atom } from "recoil";
+
+export const loaderState = atom({
+  key: "Loader",
+  default: [],
+});
---
+++ client/src/recoil/user/index.js
@@ -0,0 +1,18 @@
+import { atom, selector } from "recoil";
+
+export const userState = atom({
+  key: "User",
+  default: {
+    email: "",
+    firstName: "",
+    lastName: "",
+    isActivated: false,
+    isLoggedIn: false,
+  },
+});
+
+export const userSelector = selector({
+  key: "UserSelector",
+  get: ({ get }) => get(userState),
+  set: ({ set }, newValue) => set(userState, { ...newValue, isLoggedIn: true }),
+});
