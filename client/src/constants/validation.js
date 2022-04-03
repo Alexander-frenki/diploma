886aef991131ea5b8878450cd058223ffb9ed0cf
Oleksandr Raspopov <oleksandr.raspopov@infopulse.com>
2022-04-03 21:29:16 +0300

    add(client): validation hook, constants

---
+++ client/src/constants/validation.js
@@ -0,0 +1,6 @@
+export const MAX_CHARS = 255;
+export const PASSWORD_MIN_LENGTH = 8;
+
+export const EMAIL_REGEX =
+  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+export const USER_DATA_REGEX = /^[А-ЯІЄЇҐа-яієїґ'][А-ЯІЄЇҐа-яієїґ'\- ]+$/;
---
+++ client/src/hooks/useValidation/index.js
@@ -0,0 +1,51 @@
+import PropTypes from "prop-types";
+import { string, object, ref } from "yup";
+import {
+  EMAIL_REGEX,
+  MAX_CHARS,
+  PASSWORD_MIN_LENGTH,
+  USER_DATA_REGEX,
+} from "../../constants/validation";
+
+const EMAIL_SCHEMA = string()
+  .required("Поле обов’язкове для заповнення")
+  .max(
+    MAX_CHARS,
+    `Адреса електронної пошти має містити не більше ${MAX_CHARS} символів`
+  )
+  .matches(EMAIL_REGEX, "Будь ласка, введіть свій адрес електронної пошти");
+const PASSWORD_SCHEMA = string()
+  .required("Поле обов’язкове для заповнення")
+  .min(PASSWORD_MIN_LENGTH, "Пароль має містити не менше 8 символів")
+  .max(MAX_CHARS, `Пароль має містити не більше ${MAX_CHARS} символів`);
+const REPEAT_PASSWORD_SCHEMA = string()
+  .required("Поле обов’язкове для заповнення")
+  .min(PASSWORD_MIN_LENGTH, "Пароль має містити не менше 8 символів")
+  .max(MAX_CHARS, `Пароль має містити не більше ${MAX_CHARS} символів`)
+  .oneOf([ref("password")], "Паролі не збігаються");
+const FIRST_NAME_SCHEMA = string()
+  .required("Поле обов’язкове для заповнення")
+  .max(MAX_CHARS, `Ім'я має містити не більше ${MAX_CHARS} символів`)
+  .matches(USER_DATA_REGEX, "Будь ласка, введіть своє ім'я");
+const LAST_NAME_SCHEMA = string()
+  .required("Поле обов’язкове для заповнення")
+  .max(MAX_CHARS)
+  .matches(USER_DATA_REGEX, "Будь ласка, введіть своє прізвище");
+
+const SCHEMAS = {
+  email: EMAIL_SCHEMA,
+  password: PASSWORD_SCHEMA,
+  firstName: FIRST_NAME_SCHEMA,
+  lastName: LAST_NAME_SCHEMA,
+  repeatPassword: REPEAT_PASSWORD_SCHEMA,
+};
+
+export function useValidation(fields) {
+  return object({
+    ...fields.reduce((acc, field) => ({ ...acc, [field]: SCHEMAS[field] }), {}),
+  }).required();
+}
+
+useValidation.propTypes = {
+  fields: PropTypes.arrayOf(string).isRequired,
+};
