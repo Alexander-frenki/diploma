78eafd27b9ebaf2e57a1eb2b4e045f0868ec1708
Oleksandr Raspopov <oleksandr.raspopov@infopulse.com>
2022-05-27 20:38:20 +0300

    update(*): inject sanitaizer

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import mongoose from "mongoose";
+import expressSanitized from "express-sanitize-escape";
 import { router } from "./router/index.js";
 import { errorMiddleware } from "./middlewares/error.js";
 
@@ -12,6 +13,7 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(express.json());
+app.use(expressSanitized.middleware());
 app.use(cookieParser());
 app.use(
   cors({
